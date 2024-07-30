ed6bcd6796a38c3178c021a5c28664c0567e6371
Parham sadeghifar <parhamsadeghifar8@gmail.com>
2024-07-30 20:22:22 +0330

    Update Parham2.py

--- Parham2.py
+++ Parham2.py
@@ -4,10 +4,10 @@ laki.shape("turtle")
 laki.color('red')
 laki.width(3)
 laki.pendown
-laki.forward('180')
-laki.left('90')
-laki.forward("180")
-laki.left('90')
+laki.forward(180)
+laki.left(90)
+laki.forward(180)
+laki.left(90)
 
 
 
